35264197dde0321966945a7866a11a22f5715d12
Oscar Eduardo <oscargon12@gmail.com>
2020-02-24 23:47:59 -0500

    inicio curso react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Name = "Oscar Gonzalez";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const user1 = {
+  name: "Oscar Gonzalez",
+  age: 33,
+  Country: "Colombia"
+};
+
+const App = <h1>Tengo {user1.age}</h1>;
+
+const root = document.getElementById("root");
+
+ReactDOM.render(App, root);
